562f55c03d8ca4a2d7dc12521f2e5e0563781b7b
Nicolas Jelsch <nikks@hotmail.fr>
2018-08-17 09:53:09 +0200

    ajout tables

---
+++ src/Repository/RdvRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Rdv;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Rdv|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Rdv|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Rdv[]    findAll()
+ * @method Rdv[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RdvRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Rdv::class);
+    }
+
+//    /**
+//     * @return Rdv[] Returns an array of Rdv objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('r.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Rdv
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
